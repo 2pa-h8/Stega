fdfb7ad77f710da1499be55c2a757f6b2fcf7577
Bogdan Kozhukhov <b.kozhukhov@toolstrek.ru>
2022-03-27 16:59:02 +0300

    add fields to Controller

--- src/com/project/Controller.java
+++ src/com/project/Controller.java
@@ -1,60 +1,33 @@
 package com.project;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.embed.swing.SwingFXUtils;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.image.WritableImage;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
-
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 public class Controller {
 
-    @FXML
-    private ImageView sourceImage = new ImageView();
+    @FXML private Button embedWatermarkBtn;
+    @FXML private Button readWatermarkBtn;
+    @FXML private Button saveEmbeddingResultBtn;
+    @FXML private Button saveReadResultBtn;
+    @FXML private Button selectOriginalImageBtn;
+    @FXML private Button selectWatermarkBtn;
+    @FXML private Button selectFilledImageBtn;
 
-    @FXML
-    private ImageView outputImage = new ImageView();
+    @FXML private TextField startPspTextField1;
+    @FXML private TextField startPspTextField2;
 
-    @FXML
-    private TextField encodingText;
+    @FXML private ImageView originalImageView = new ImageView();
+    @FXML private ImageView watermarkOriginalView = new ImageView();
+    @FXML private ImageView watermarkReadView = new ImageView();
+    @FXML private ImageView embeddingResultView = new ImageView();
+    @FXML private ImageView readingResultView = new ImageView();
 
-    @FXML
-    private Button encodeBtn;
-
-    @FXML
-    private Button openImageBtn;
-
-    @FXML
-    private Button saveImageBtn;
-
-    @FXML
-    private Button readImageBtn;
-
-    @FXML
-    private ComboBox< String > algorithmList;
-
-    private Stage _stage;
-    private Image _sourceImage;
-    private String _sourceImageFileName;
-    private String _sourceImagePath;
-    private WritableImage _outputImage;
+    @FXML private Stage stage;
 
     public void init( Stage primaryStage ) {
-        _stage = primaryStage;
+        stage = primaryStage;
     }
 }
